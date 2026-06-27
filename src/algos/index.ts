0673a969098812f0e34c2da041a5d24e27c901b0
Renan Andrade de Gouveia <ag.renan@hotmail.com>
2026-06-27 17:06:42 -0300

    Refactor: Changing algo name

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,13 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as whatsAlf from './whats-alf'
-import * as ptRegex from './pt-regex'
+import * as ragFeed from './rag-feed'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [whatsAlf.shortname]: whatsAlf.handler,
-  [ptRegex.shortname]: ptRegex.handler,
+  [ragFeed.shortname]: ragFeed.handler,
 }
 
 export default algos
